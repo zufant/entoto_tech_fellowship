b4ff01b13869a5b9237c969dd19540d7b1054193
zufant <zufantadese1212@gmail.com>
2019-06-06 12:18:39 +0300

    add file 3

---
+++ week1/dayy3.js
@@ -0,0 +1,22 @@
+//for(var i = 1; i < 10; i++){
+  //console.log(i)
+//}
+var book = {
+  title: "chemistry",
+  auther: "zufan",
+  releaseDate: "12-13-2019"
+}
+//console.log(book)
+//console.log(book.title)
+//var food = ["injera","food","bread"]
+//console.log(food)
+//for(i = 0; i < 3; i++){
+  //console.log(food[ i])
+  
+//}
+var i =  0;
+while(i < 10){
+   i = i + 1;
+    console.log(i)
+  
+}
