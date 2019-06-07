cdb7fc2abea4572191f2b95aefa56ab2150f856f
zufant <zufantadese1212@gmail.com>
2019-06-07 10:16:00 +0300

    add day5 file

---
+++ week1/day5.js
@@ -0,0 +1,14 @@
+for(var num = 1; num <=100; num++){
+  if(num % 3 == 0){
+    console.log(num +" "+"Fizz")
+  } 
+  else if(num % 5 == 0){
+    console.log(num +" "+"Bizz")
+  }
+   else if(num % 5 == 0 && num % 3 == 0){
+    console.log(num +" "+"FizzBizz")
+  }
+    else
+      console.log(num +" "+ "it is not divisible by 3 and 5")
+  
+}
