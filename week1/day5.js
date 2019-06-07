68944422da3dd96e22321107f6374b4551b337d4
zufant <zufantadese1212@gmail.com>
2019-06-07 10:25:16 +0300

    update file 4

--- week1/day5.js
+++ week1/day5.js
@@ -1,14 +1,16 @@
 for(var num = 1; num <=100; num++){
-  if(num % 3 == 0){
+   if(num % 5 == 0 && num % 3 == 0){
+    console.log(num +" "+"FizzBizz")
+  }
+ else if(num % 3 == 0){
     console.log(num +" "+"Fizz")
   } 
   else if(num % 5 == 0){
     console.log(num +" "+"Bizz")
   }
-   else if(num % 5 == 0 && num % 3 == 0){
-    console.log(num +" "+"FizzBizz")
-  }
-    else
+    
+  else
       console.log(num +" "+ "it is not divisible by 3 and 5")
+  
   
 }
